337f51cc8616e4e06f79796021beb946a87ffc89
MAKOTO OBA <86038701+TokomaBaou@users.noreply.github.com>
2022-05-10 21:59:40 +0900

    screenA

---
+++ src/screenA.tsx
@@ -0,0 +1,13 @@
+import { useHistory } from "react-router-dom";
+
+export const ScreenA = () => {
+  const history = useHistory();
+  const handleClick = (params: any) => {
+    history.push({ pathname: "/", state: params });
+  };
+  return (
+    <div>
+      <button onClick={() => handleClick(data)}>ボタン</button>
+    </div>
+  );
+};
